7e59ef819cb8effd5284433498cca25c87f523e8
omarhu12 <94450924+omarhu12@users.noreply.github.com>
2022-08-12 12:04:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
 // splash screen
 document.querySelector(".controlBtns button").onclick = function () {
   document.querySelector(".controlBtns").remove();
+  blocks.forEach((block) => {
+    block.classList.add("show");
+  });
+  //   console.log(
+  //     blocks.every((block) => {
+  //       return block.classList.contains("hasMatched");
+  //     })
+  //   );
+
+  setTimeout(() => {
+    blocks.forEach((block) => {
+      block.classList.remove("show");
+    });
+  }, duration * 2);
 };
 
 let triesElement = document.querySelector(".tries span");
 
 let newGame = document.querySelector(".newGameButton");
+let endGameNewGame = document.querySelector(".endNew");
+let endGame = document.querySelector(".win");
 
 let restartGame = document.querySelectorAll(".gameBlock");
 console.log(restartGame);
@@ -20,12 +36,24 @@ newGame.addEventListener("click", function () {
   });
   triesElement.innerHTML = 0;
 });
+endGameNewGame.addEventListener("click", function () {
+  //   restartGame.classList.add("restart");
+  //   restartGame.classList.remove("hasMatched");
+
+  blocks.forEach((block) => {
+    block.classList.remove("show");
+    block.classList.remove("hasMatched");
+  });
+  triesElement.innerHTML = 0;
+  endGame.classList.remove("show");
+});
 
 let duration = 1000;
 
 let blocksContainer = document.querySelector(".memoryGameBlocks");
 
 let blocks = Array.from(blocksContainer.children);
+console.log(blocks);
 
 let orderRange = [...Array(blocks.length).keys()];
 
@@ -51,6 +79,14 @@ function flipBlock(selectedBlock) {
   if (allFlippedBlocks.length === 2) {
     stopClicking();
     checkMatcedBlock(allFlippedBlocks[0], allFlippedBlocks[1]);
+  }
+
+  if (
+    blocks.every((block) => {
+      return block.classList.contains("hasMatched");
+    })
+  ) {
+    endGame.classList.add("show");
   }
 }
 
